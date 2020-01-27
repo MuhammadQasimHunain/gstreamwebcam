1b528e37504c7852fafa3f4677083c6e7cc97d65
Muhammad Qasim Hunain <muhammad.qasim1255@hotmail.com>
2020-01-27 20:27:51 +0500

    file location fix

--- gstreamcam.py
+++ gstreamcam.py
@@ -45,13 +45,13 @@ def createsdp(hostname, streams):
 def main(arguments):
     
     gstreamer = 'gst-launch-1.0.exe' if 'win' in sys.platform else 'gst-launch-1.0'
-    hostname = arguments.hostname
+    hostname = 'localhost'
     encoders = {'h264' : (b'GstRtpH264Pay' , 'x264enc', 'rtph264pay'),
                 'vp8' : (b'GstRtpVP8Pay', 'vp8enc', 'rtpvp8pay'),
                 'openh264' : (b'GstRtpH264Pay', 'openh264enc', 'rtph264pay')}
     rtppay = encoders[arguments.codec][0]
     port = arguments.port
-    arglist = [gstreamer, "-v", "ksvideosrc", "device_index=%d" % arguments.camera, "!", "videoconvert", "!", "videoscale", "!", "video/x-raw,width=800,height=600", "!",
+    arglist = [gstreamer, "-v", "ksvideosrc", "filesrc location=%s" % arguments.videosrc, "!", "videoconvert", "!", "videoscale", "!", "video/x-raw,width=800,height=600", "!",
                     encoders[arguments.codec][1],  "!", encoders[arguments.codec][2], "!", "udpsink", "host=%s" % hostname, "port=%d" % port]
     if arguments.debug:
         print("Calling gstreamer:\n"," ".join(arglist))
@@ -86,16 +86,15 @@ def main(arguments):
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("hostname", help="hostname or IP address of the destination")
     parser.add_argument("--sdp", help="generates SDP file for the stream (defaults to false)", action="store_true")
     parser.add_argument("--debug", help="shows command line in use to call gstreamer", action="store_true")
     parser.add_argument("--port", "-p", help="port (defaults to 5000)", type=int, default=5000)
     parser.add_argument("--codec", help="chooses encoder (defaults to openh264)", choices=['vp8', 'h264', 'openh264'], default='openh264')
-    parser.add_argument("--camera", help="Device id (defaults to 0)", type=int, default=0)
+    parser.add_argument("--videosrc", help="Device id (defaults to 0)", default='test.mp4')
 
     args = parser.parse_args()
 
-    args.hostname = socket.gethostbyname(args.hostname)
-    print("Using hostname %s using device %d" % (args.hostname, args.camera))
+    args.hostname = socket.gethostbyname('localhost')
+    print("Using hostname %s using file %s" % (args.hostname, args.videosrc))
 
     main(args)
